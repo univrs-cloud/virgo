d7b25351237b7df13b065e067fdb16454518e46c
Robert <36549+VeloAddict@users.noreply.github.com>
2023-11-18 19:49:19 +0200

    Update 06-run.sh

--- stage2/06-ups/06-run.sh
+++ stage2/06-ups/06-run.sh
@@ -6,3 +6,8 @@ fi
 
 install -v -m 755 files/ups.shutdown "${ROOTFS_DIR}/lib/systemd/system-shutdown/"
 install -v -m 755 files/ups.service "${ROOTFS_DIR}/lib/systemd/system/"
+install -v -m 755 files/ups.sh "${ROOTFS_DIR}/usr/sbin/"
+
+on_chroot << EOF
+systemctl enable ups.service
+EOF
