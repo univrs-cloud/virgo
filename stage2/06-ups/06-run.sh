b0d2fa56f91f2de187b823fa370b060f53e0e386
Robert <robert.gurde@gmail.com>
2023-11-18 14:37:09 +0000

    Power off UPS when system is shut down

--- stage2/06-ups/06-run.sh
+++ stage2/06-ups/06-run.sh
@@ -3,3 +3,5 @@
 if ! grep -q "^i2c[-_]dev" "${ROOTFS_DIR}/etc/modules"; then
     printf "i2c-dev\n" >> "${ROOTFS_DIR}/etc/modules"
 fi
+
+install -v -m 755 files/ups.shutdown "${ROOTFS_DIR}/lib/systemd/system-shutdown/"
