24446d506d85c9aa4026046c34c804de9ab22c68
Robert <36549+VeloAddict@users.noreply.github.com>
2023-12-13 10:58:20 +0200

    Update 05-run.sh

--- stage2/05-docker/05-run.sh
+++ stage2/05-docker/05-run.sh
@@ -21,7 +21,4 @@ check_internet
 curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
 usermod -aG docker $FIRST_USER_NAME || error "Failed to add user to the Docker usergroup."
 systemctl enable docker
-
-#docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-#docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
 EOF
