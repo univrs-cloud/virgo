7ded9645e285c5685b7ce81b1e75232676ef4a28
Robert <36549+VeloAddict@users.noreply.github.com>
2024-01-11 00:56:43 +0200

    Update ups.sh

--- stage2/06-ups/files/ups.sh
+++ stage2/06-ups/files/ups.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Expects to be launched in the blackbox ups project dir (usually: /usr/share/blackbox/)
-/usr/bin/python3 -m monolith.ups.service
+# Expects to be launched in the virgo ups project dir (usually: /usr/share/virgo/)
+/usr/bin/python3 -m virgo.ups.service
