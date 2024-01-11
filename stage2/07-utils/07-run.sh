33e56947968d4faec8d824bc38e0398d67845b2a
Robert <36549+VeloAddict@users.noreply.github.com>
2024-01-11 14:02:54 +0200

    Update 07-run.sh

--- stage2/07-utils/07-run.sh
+++ stage2/07-utils/07-run.sh
@@ -7,8 +7,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source /root/.bashrc
 nvm install --lts
 
-sudo ln -s /root/.nvm/versions/node/v20.10.0/bin/node /usr/bin/node
-sudo ln -s /root/.nvm/versions/node/v20.10.0/bin/npm /usr/bin/npm
+sudo ln -s /root/.nvm/versions/node/v20.11.0/bin/node /usr/bin/node
+sudo ln -s /root/.nvm/versions/node/v20.11.0/bin/npm /usr/bin/npm
 
 dpkg -i /tmp/virgo-stats_1.0.0_all.deb
 EOF
