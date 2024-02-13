42ff4bef82bc0c56a67cb16a448a1cfbb831b63f
Robert <robert.gurde@gmail.com>
2024-02-13 19:13:38 +0000

    Update utils

--- stage2/07-utils/07-run.sh
+++ stage2/07-utils/07-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-install -v -m 644 files/virgo-stats_1.0.0_all.deb "${ROOTFS_DIR}/tmp/virgo-stats_1.0.0_all.deb"
+install -v -m 644 files/virgo-api_1.0.0_all.deb "${ROOTFS_DIR}/tmp/virgo-api_1.0.0_all.deb"
+install -v -m 644 files/virgo-ui_1.0.0_all.deb "${ROOTFS_DIR}/tmp/virgo-ui_1.0.0_all.deb"
 
 on_chroot << EOF
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -10,5 +11,6 @@ nvm install --lts
 sudo ln -s /root/.nvm/versions/node/v20.11.0/bin/node /usr/bin/node
 sudo ln -s /root/.nvm/versions/node/v20.11.0/bin/npm /usr/bin/npm
 
-dpkg -i /tmp/virgo-stats_1.0.0_all.deb
+dpkg -i /tmp/virgo-api_1.0.0_all.deb
+dpkg -i /tmp/virgo-ui_1.0.0_all.deb
 EOF
