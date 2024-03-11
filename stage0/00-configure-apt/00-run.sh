d3a1dd135d3a9b5e21161e2b97cda15b2470757c
Robert <robert.gurde@gmail.com>
2024-03-11 19:10:03 +0000

    Add backports for ZFS

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -5,6 +5,10 @@ install -m 644 files/raspi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/raspi.list"
 
+install -m 644 files/bookworm-backports.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -v -d "${ROOTFS_DIR}/etc/apt/preferences.d"
+install -v -m 644 files/90_zfs "${ROOTFS_DIR}/etc/apt/preferences.d/"
+
 if [ -n "$APT_PROXY" ]; then
 	install -m 644 files/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 	sed "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache" -i -e "s|APT_PROXY|${APT_PROXY}|"
