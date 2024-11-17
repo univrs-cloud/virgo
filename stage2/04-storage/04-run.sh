7f7d29e2e50787163c8debd486a75c430aa155d0
Robert <36549+VeloAddict@users.noreply.github.com>
2024-11-17 21:41:22 +0200

    Install zfs.conf into /etc/modprobe.d/

--- stage2/04-storage/04-run.sh
+++ stage2/04-storage/04-run.sh
@@ -6,4 +6,6 @@ install -v -m 600 files/multipath.conf "${ROOTFS_DIR}/etc/multipath/conf.d/"
 install -v -d "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/udisks2/modules"
 install -v -m 600 files/empty "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/udisks2/modules/"
 
+install -v -m 644 files/zfs.conf "${ROOTFS_DIR}/etc/modprobe.d/"
+
 install -v -m 644 files/smb.conf "${ROOTFS_DIR}/etc/samba/"
