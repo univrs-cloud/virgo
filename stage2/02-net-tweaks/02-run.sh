dc63f6b93f12120718285e32ddcbdb22adb4437b
Robert <36549+VeloAddict@users.noreply.github.com>
2025-04-19 00:08:52 +0300

    Update 02-run.sh

--- stage2/02-net-tweaks/02-run.sh
+++ stage2/02-net-tweaks/02-run.sh
@@ -1,1 +1,8 @@
+#!/bin/bash -e
 
+install -v -m 644 files/fail2ban.local "${ROOTFS_DIR}/etc/fail2ban/fail2ban.local"
+install -v -m 644 files/jail.local "${ROOTFS_DIR}/etc/fail2ban/jail.local"
+
+on_chroot << EOF
+systemctl enable fail2ban
+EOF
