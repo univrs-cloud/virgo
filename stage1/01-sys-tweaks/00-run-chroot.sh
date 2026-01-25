dd50430621504f0f70bf4241fab93482bd024de0
Robert <robert.gurde@gmail.com>
2026-01-25 10:44:01 +0000

    Configure journald

---
+++ stage1/01-sys-tweaks/00-run-chroot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+sed -i 's/^#Compress=.*/Compress=yes/' /etc/systemd/journald.conf
+sed -i 's/^#SystemMaxUse=.*/SystemMaxUse=50M/' /etc/systemd/journald.conf
+sed -i 's/^#SyncIntervalSec=.*/SyncIntervalSec=5m/' /etc/systemd/journald.conf
