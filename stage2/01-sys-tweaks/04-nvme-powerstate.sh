cea97d7e9f5887d3ed573eb4beea77feaa361b26
Robert <robert.gurde@gmail.com>
2026-02-08 13:15:01 +0000

    Update nvme powerstate udev rules

---
+++ stage2/01-sys-tweaks/04-nvme-powerstate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+install -m 644 files/99-nvme-powerstate.rules "${ROOTFS_DIR}/usr/lib/udev/rules.d/99-nvme-powerstate.rules"
--- stage2/01-sys-tweaks/04-nvmw-powersave.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-install -m 644 files/99-nvme-powersave.rules "${ROOTFS_DIR}/etc/udev/rules.d/99-nvme-powersave.rules"
