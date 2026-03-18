6a71bb14b94c3f8339cf23ffe059a345763070cc
Robert <36549+VeloAddict@users.noreply.github.com>
2026-03-18 12:30:34 +0200

    Update n version from 22 to 24

--- stage2/08-utils/00-run.sh
+++ stage2/08-utils/00-run.sh
@@ -5,7 +5,7 @@ install -v -d "${ROOTFS_DIR}/var/log/pcp/pmlogger"
 on_chroot << EOF
 curl -fsSL -o /usr/local/bin/n https://raw.githubusercontent.com/tj/n/master/bin/n 
 chmod 0755 /usr/local/bin/n
-n 22
+n 24
 
 systemctl enable redis-server
 systemctl disable pmlogger_check.timer pmlogger_daily.timer pmlogger_farm_check.timer
