b49fb057de8c6592afa0d9d9271cc13e32e6791f
Robert <36549+VeloAddict@users.noreply.github.com>
2026-05-12 14:34:55 +0300

    Remove multipath configuration installation
    Removed installation of multipath configuration files.

--- stage2/05-storage/00-run.sh
+++ stage2/05-storage/00-run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-install -v -d "${ROOTFS_DIR}/etc/multipath/conf.d"
-install -v -m 600 files/multipath.conf "${ROOTFS_DIR}/etc/multipath/conf.d/"
-
 install -v -d "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/udisks2/modules"
 install -v -m 600 files/empty "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/udisks2/modules/"
 
