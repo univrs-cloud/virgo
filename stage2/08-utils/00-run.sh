0c035ed81b2f321f0472f507f9155a00172fbf0f
Robert <36549+VeloAddict@users.noreply.github.com>
2026-05-14 14:50:22 +0300

    Remove pmlogger installation and service management
    Removed installation of pmlogger and related timers from script.

--- stage2/08-utils/00-run.sh
+++ stage2/08-utils/00-run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -e
 
-install -v -d "${ROOTFS_DIR}/var/log/pcp/pmlogger"
-
 on_chroot << EOF
 curl -fsSL -o /usr/local/bin/n https://raw.githubusercontent.com/tj/n/master/bin/n 
 chmod 0755 /usr/local/bin/n
 n 24
 
 systemctl enable redis-server
-systemctl disable pmlogger_check.timer pmlogger_daily.timer pmlogger_farm_check.timer
-systemctl disable pmcd pmlogger.service pmlogger_farm.service pmproxy.service
 EOF
