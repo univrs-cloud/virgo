bfb787ef1f14a5018f74d0e83c04ce74d7d5fdf1
Robert <robert.gurde@gmail.com>
2026-09-03 12:37:58 +0300

    Reannounce mdns

--- stage2/02-net-tweaks/02-run.sh
+++ stage2/02-net-tweaks/02-run.sh
@@ -2,3 +2,4 @@
 
 mkdir -p "${ROOTFS_DIR}/etc/NetworkManager/dispatcher.d/pre-up.d"
 install -v -m 755 files/wait-for-gateway "${ROOTFS_DIR}/etc/NetworkManager/dispatcher.d/pre-up.d/10-wait-for-gateway"
+install -v -m 755 files/reannounce-mdns "${ROOTFS_DIR}/etc/NetworkManager/dispatcher.d/50-reannounce-mdns"
