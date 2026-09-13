fffd11cefb59d70b1f6e6e00918f67eaf8909b3e
Robert <robert.gurde@gmail.com>
2026-09-13 20:26:01 +0300

    Don't install nvme power state rules, not needed anymoe

--- stage2/01-sys-tweaks/02-run.sh
+++ stage2/01-sys-tweaks/02-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
-#install -m 644 files/99-nvme-powerstate.rules "${ROOTFS_DIR}/usr/lib/udev/rules.d/99-nvme-powerstate.rules"
+# Install rpi-swap drop-in to configure zram-only swap (no backing file)
+install -d "${ROOTFS_DIR}/etc/rpi/swap.conf.d"
+install -m 644 files/90-zram-only.conf "${ROOTFS_DIR}/etc/rpi/swap.conf.d/90-zram-only.conf"
