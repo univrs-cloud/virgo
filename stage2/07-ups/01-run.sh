f818193f8f6dc2c1934a0e02e846b51bb744ba99
Robert <robert.gurde@gmail.com>
2026-09-16 13:47:17 +0300

    feat(ups): start the driver the moment a UPS is plugged in

--- stage2/07-ups/01-run.sh
+++ stage2/07-ups/01-run.sh
@@ -4,3 +4,5 @@ install -v -m 640 -o root -g nut files/nut.conf files/ups.conf files/upsd.conf f
 
 install -v -d "${ROOTFS_DIR}/etc/systemd/system/nut-driver@.service.d"
 install -v -m 644 files/retry.conf "${ROOTFS_DIR}/etc/systemd/system/nut-driver@.service.d/"
+
+install -v -m 644 files/63-univrs-nut-hotplug.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
