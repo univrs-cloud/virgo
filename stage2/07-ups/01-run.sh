c1da7be7e9b3310d5a3d76d0cc92767fceff688b
Robert <robert.gurde@gmail.com>
2026-09-16 12:55:04 +0300

    feat(ups): find a USB UPS on its own and come back when power does

---
+++ stage2/07-ups/01-run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+install -v -m 640 -o root -g nut files/nut.conf files/ups.conf files/upsd.conf files/upsd.users files/upsmon.conf "${ROOTFS_DIR}/etc/nut/"
+
+install -v -d "${ROOTFS_DIR}/etc/systemd/system/nut-driver@.service.d"
+install -v -m 644 files/retry.conf "${ROOTFS_DIR}/etc/systemd/system/nut-driver@.service.d/"
